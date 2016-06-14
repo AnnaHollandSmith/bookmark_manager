0b562dfeaff775d343f317e9a14f3a64416a2507
Anna <anna.hollandsmith3@gmail.com>
2016-06-14 13:50:03 +0100

    added name column to existing database

--- app/models/link.rb
+++ app/models/link.rb
@@ -5,6 +5,7 @@ class Link
   include DataMapper::Resource
 
   property :id, Serial
+  property :name, String
   property :url, String
 end
 
--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -1,10 +1,16 @@
 #require "../../app"
-require "capybara/rspec"
+#require "capybara/rspec"
+
+#As a time-pressed user
+#So that I can quickly go to web sites I regularly visit
+#I would like to see a list of links on the homepage
 
 
 feature 'Viewing links' do
+  Link.create(url: " http://www.google.co.uk", name: 'google')
   scenario 'user visits homepage and can see links' do
     visit '/'
-    expect(page).to have_content('Link1')
+    expect(page.status_code).to eq 200
+    expect(page).to have_content('www.google.co.uk')
   end
 end
