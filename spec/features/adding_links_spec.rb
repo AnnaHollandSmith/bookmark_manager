290beb520476c1aad6960265ed402224c7a0026c
Anna <anna.hollandsmith3@gmail.com>
2016-06-14 14:24:43 +0100

    created a create link page to add links to the database and redirect to root

---
+++ spec/features/adding_links_spec.rb
@@ -0,0 +1,14 @@
+# As a time-pressed user
+# So that I can save a website
+# I would like to add the site's address and title to my bookmark manager
+
+feature 'user can add link to homepage' do
+  scenario 'user adds site address and title to their bookmark manager' do
+    visit '/add_link'
+    fill_in 'name', with: 'This is BBC'
+    fill_in 'url', with: 'www.bbc.co.uk'
+    click_button 'Add link'
+    expect(current_path). to eq '/'
+    expect(page).to have_content ('This is BBC')
+  end
+end
