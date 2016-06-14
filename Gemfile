c7de6b13779555f0175bc5802e0bcef9214e6c31
Anna <anna.hollandsmith3@gmail.com>
2016-06-14 11:32:44 +0100

    initialized rspec-sinatra, built link class

--- Gemfile
+++ Gemfile
@@ -3,5 +3,7 @@ source "https://rubygems.org"
 
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
-
+gem 'rspec'
+gem 'capybara'
+gem 'rspec-sinatra'
 # gem "rails"
---
+++ spec/features/viewing_links_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+feature 'Viewing links' do
+  scenario 'user visits homepage and can see links' do
+    visit '/'
+    expect(page).to have_content('Link1')
+  end
+end
