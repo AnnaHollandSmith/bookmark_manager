9b247aca768503404a3439e75e1b6632520a4c5e
Anna <anna.hollandsmith3@gmail.com>
2016-06-17 12:30:29 +0100

    preventing duplicate registration using flash

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -21,15 +21,23 @@ feature 'User sign up' do
 
   scenario 'with a password that does not match' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
-    expect(current_path).to eq('/users') # current_path is a helper provided by Capybara
-    expect(page).to have_content 'Password and confirmation password do not match'
+    expect(current_path).to eq('/users') 
+    expect(page).to have_content 'assword does not match the confirmation'
   end
 
   scenario "I can't sign up without an email address" do
     expect { sign_up(email: nil) }.not_to change(User, :count)
+    expect(page).to have_content('Email must not be blank')
   end
 
-  scenario " I can't sign up with an invalid email address" do
+  scenario "I can't sign up with an invalid email address" do
     expect { sign_up(email: 'invalid@email') }.to change(User, :count).by(0)
+    expect(page).to have_content('Email has an invalid format')
+  end
+
+  scenario "I cannot sign up with an existing email address" do
+    sign_up
+    expect { sign_up }.to_not change(User, :count)
+    expect(page).to have_content('Email is already taken')
   end
 end
